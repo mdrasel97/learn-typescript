7d7398d2a5d76a4f573d6d85db89cb78c6dd497e
mdrasel97 <mollarasel972@gmail.com>
2025-11-09 17:57:03 +0600

    primitive array

---
+++ src/nonPrimitive.ts
@@ -0,0 +1,11 @@
+// non Primitive: Array, Object
+
+let bazarList: string[] = ['eggs', 'milk', 'butter'];
+bazarList.push(12);
+
+let mixedArr: (string | number | boolean)[] = ['eggs', 2, true];
+mixedArr.push(true)
+
+let Coordinates: [number, number] = [34, 45];
+
+let raselNameAndAge : [string, number] = ['rasel', 25];
