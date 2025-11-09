f26ebf819627270c64c6f5d6c23b0960918945b9
mdrasel97 <mollarasel972@gmail.com>
2025-11-09 18:23:27 +0600

    nonPrimitive object

--- src/nonPrimitive.ts
+++ src/nonPrimitive.ts
@@ -9,3 +9,37 @@ mixedArr.push(true)
 let Coordinates: [number, number] = [34, 45];
 
 let raselNameAndAge : [string, number] = ['rasel', 25];
+
+
+// const user: {
+//     organization: 'programming hero',
+//     isMarried: boolean,
+//     firstName: string,
+//     middleName: string,
+//     lastName?: string //optional property
+// } = {
+//     firstName: 'MD',
+//     middleName: 'RASEL',
+//     lastName: 'something',
+//     organization: 'some organization',
+//     isMarried: true,
+// }
+
+// user.organization = 'programming hero'
+const user: {
+    readonly organization: string,
+    isMarried: boolean,
+    firstName: string,
+    middleName: string,
+    lastName?: string //optional property
+} = {
+    firstName: 'MD',
+    middleName: 'RASEL',
+    lastName: 'something',
+    organization: 'some organization',
+    isMarried: true,
+}
+
+user.organization = 'programming hero'
+
+console.log(user);
